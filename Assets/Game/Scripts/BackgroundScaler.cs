cad42d838e0a6488dc52a072e602e2b2598035a8
Vinícius <vinisicius2@gmail.com>
2022-12-20 08:38:21 -0300

    Intellisense for Unity on VSCode

--- Assets/Game/Scripts/BackgroundScaler.cs
+++ Assets/Game/Scripts/BackgroundScaler.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundScaler : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 startScale;
+    private Vector3 startScale;  
     void Start()
     {
         //Pego a escala global do valor inicial(x,y,z) dentro do atributo transform q td object tem
@@ -18,6 +18,7 @@ public class BackgroundScaler : MonoBehaviour
 
         //Atribui isso ao localScale
         transform.localScale = new Vector3(width, height, startScale.z);
+        
 
     }
 

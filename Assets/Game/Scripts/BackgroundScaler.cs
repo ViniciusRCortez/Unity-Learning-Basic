4f5487198e60b7c9999548d1e65015cdd27515b8
Vinícius <vinisicius2@gmail.com>
2022-12-20 22:17:52 -0300

    Ajuste no BackgroundScaler para conseguir definir em qual dimensão vou redimensionar, assim como ajuste nos parametros e um codigo pra textura dinamica

--- Assets/Game/Scripts/BackgroundScaler.cs
+++ Assets/Game/Scripts/BackgroundScaler.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class BackgroundScaler : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 startScale;  
+    private Vector3 startScale;
+
+    public enum ScaleTypes 
+    {
+        x,
+        y,
+        all
+    }  
+
+    public ScaleTypes scaleType = ScaleTypes.all; 
     void Start()
+    {        
+        FitToScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FitToScreen()
     {
         //Pego a escala global do valor inicial(x,y,z) dentro do atributo transform q td object tem
         startScale = transform.localScale;
@@ -16,15 +36,14 @@ public class BackgroundScaler : MonoBehaviour
         //Normaliza a altura pela nova largura
         float width = height*Screen.width/Screen.height;
 
+        float newWidth;
+        float newHeight;
+        
+        //Escolha da logica de qual dimensão aplicar scale 
+        newWidth = ( scaleType == ScaleTypes.x || scaleType == ScaleTypes.all ) ? width : startScale.x;
+        newHeight = ( scaleType == ScaleTypes.y || scaleType == ScaleTypes.all ) ? height : startScale.y;
+
         //Atribui isso ao localScale
-        transform.localScale = new Vector3(width, height, startScale.z);
-        
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.localScale = new Vector3(newWidth, newHeight, startScale.z);
     }
 }
